614190d92b4986b975c9379976ceceaaf7c19fb7
Stephen Williams <stephenjw@fastmail.com>
2021-08-27 12:52:41 -0400

    HS -> RS

--- src/main/java/com/sjwi/meals/service/security/JwtManager.java
+++ src/main/java/com/sjwi/meals/service/security/JwtManager.java
@@ -1,7 +1,6 @@
 package com.sjwi.meals.service.security;
 
-import java.security.NoSuchAlgorithmException;
-import java.security.spec.InvalidKeySpecException;
+import java.util.Base64;
 
 import javax.crypto.spec.SecretKeySpec;
 import javax.xml.bind.DatatypeConverter;
@@ -11,12 +10,8 @@ import com.sjwi.meals.model.AccessTokenResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.SignatureException;
-import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.impl.crypto.DefaultJwtSignatureValidator;
 
 @Component
 public class JwtManager {
@@ -24,10 +19,20 @@ public class JwtManager {
   @Value("${meals.auth.client_secret}")
   String clientSecret;
 
-  public String getUnpackedAccessToken(AccessTokenResponse response) throws NoSuchAlgorithmException, ExpiredJwtException, UnsupportedJwtException, MalformedJwtException, SignatureException, IllegalArgumentException, InvalidKeySpecException {
+  public String getUnpackedAccessToken(AccessTokenResponse response) throws Exception {
     SignatureAlgorithm sa = SignatureAlgorithm.RS256;
     SecretKeySpec secretKeySpec = new SecretKeySpec(DatatypeConverter.parseBase64Binary(clientSecret), sa.getJcaName());
-    return Jwts.parser().setSigningKey(secretKeySpec).parseClaimsJws(response.getAccess_token()).getBody().toString();
+    String[] chunks = response.getAccess_token().split("\\.");
+    Base64.Decoder decoder = Base64.getDecoder();
+    String header = new String(decoder.decode(chunks[0]));
+    String payload = new String(decoder.decode(chunks[1]));
+    String tokenWithoutSignature = chunks[0] + "." + chunks[1];
+    String signature = chunks[2];
+    DefaultJwtSignatureValidator validator = new DefaultJwtSignatureValidator(sa, secretKeySpec);
+    if (!validator.isValid(tokenWithoutSignature, signature)) {
+      throw new Exception("Could not verify JWT token integrity!");
+    }
+    return payload;
   }
   
 }
