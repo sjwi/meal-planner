860d17491abbeb6cb1cc844ea4ce4e7ced508b30
Stephen Williams <stephenjw@fastmail.com>
2021-08-27 10:11:32 -0400

    httpclient

--- src/main/java/com/sjwi/meals/service/security/OAuthManager.java
+++ src/main/java/com/sjwi/meals/service/security/OAuthManager.java
@@ -1,25 +1,17 @@
 package com.sjwi.meals.service.security;
 
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 import com.google.gson.Gson;
 import com.sjwi.meals.model.TokenRequest;
 
-import org.apache.commons.codec.binary.Base64;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.config.CookieSpecs;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.impl.client.HttpClients;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.boot.web.client.RestTemplateCustomizer;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
 
 @Component
 public class OAuthManager {
@@ -42,42 +34,77 @@ public class OAuthManager {
   @Value("${meals.auth.scopes}")
   String scopes;
 
-  public String getOAuthToken(String code) {
-    ResponseEntity<String> response = null;
-    System.out.println("Authorization Code------" + code);
-
-    RestTemplate restTemplate = new RestTemplate();
-    restTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory());
-
-    String credentials = clientId +  ":" + clientSecret;
-    System.out.println(credentials);
-    String encodedCredentials = new String(Base64.encodeBase64(credentials.getBytes()));
-
-    HttpHeaders headers = new HttpHeaders();
-    headers.setAccept(Arrays.asList(MediaType.ALL));
-    headers.setContentType(MediaType.APPLICATION_JSON);
-    headers.add("Authorization", "Basic " + encodedCredentials);
-
+  public String getOAuthToken(String code) throws Exception {
+    DefaultHttpClient httpClient = new DefaultHttpClient();
+    HttpPost postRequest = new HttpPost(tokenUrl);
 
     TokenRequest payload = new TokenRequest(code,clientId,redirectUri);
-    new Gson().toJson(payload);
+    String payloadString = new Gson().toJson(payload);
+    StringEntity input = new StringEntity(payloadString);
+    input.setContentType("application/json");
+    postRequest.setEntity(input);
 
-    HttpEntity<String> request = new HttpEntity<String>(new Gson().toJson(payload),headers);
-    response = restTemplate.postForEntity(tokenUrl, request, String.class);
-    return response.getBody();
-  }
+    HttpResponse response = httpClient.execute(postRequest);
 
-  public class RestTemplateStandardCookieCustomizer implements RestTemplateCustomizer {
-    @Override
-    public void customize(final RestTemplate restTemplate) {
-      final HttpClient httpClient = HttpClients.custom()
-          .setDefaultRequestConfig(RequestConfig.custom()
-              .setCookieSpec(CookieSpecs.STANDARD).build())
-          .build();
-
-      restTemplate.setRequestFactory(
-        new HttpComponentsClientHttpRequestFactory(httpClient)
-      );
+    if (response.getStatusLine().getStatusCode() != 201) {
+        throw new RuntimeException("Failed : HTTP error code : "
+            + response.getStatusLine().getStatusCode());
     }
+
+    BufferedReader br = new BufferedReader(
+                    new InputStreamReader((response.getEntity().getContent())));
+
+    String output;
+    while ((output = br.readLine()) != null) {
+       output += output; 
+    }
+
+    httpClient.getConnectionManager().shutdown();
+
+    return output;
+
+
+
+
+
+
+
+
+    // ResponseEntity<String> response = null;
+    // System.out.println("Authorization Code------" + code);
+
+    // RestTemplate restTemplate = new RestTemplate();
+    // restTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory());
+
+    // String credentials = clientId +  ":" + clientSecret;
+    // System.out.println(credentials);
+    // String encodedCredentials = new String(Base64.encodeBase64(credentials.getBytes()));
+
+    // HttpHeaders headers = new HttpHeaders();
+    // headers.setAccept(Arrays.asList(MediaType.ALL));
+    // headers.setContentType(MediaType.APPLICATION_JSON);
+    // headers.add("Authorization", "Basic " + encodedCredentials);
+
+
+    // TokenRequest payload = new TokenRequest(code,clientId,redirectUri);
+    // new Gson().toJson(payload);
+
+    // HttpEntity<String> request = new HttpEntity<String>(new Gson().toJson(payload),headers);
+    // response = restTemplate.postForEntity(tokenUrl, request, String.class);
+    // return response.getBody();
   }
+
+  // public class RestTemplateStandardCookieCustomizer implements RestTemplateCustomizer {
+  //   @Override
+  //   public void customize(final RestTemplate restTemplate) {
+  //     final HttpClient httpClient = HttpClients.custom()
+  //         .setDefaultRequestConfig(RequestConfig.custom()
+  //             .setCookieSpec(CookieSpecs.STANDARD).build())
+  //         .build();
+
+  //     restTemplate.setRequestFactory(
+  //       new HttpComponentsClientHttpRequestFactory(httpClient)
+  //     );
+  //   }
+  // }
 }
