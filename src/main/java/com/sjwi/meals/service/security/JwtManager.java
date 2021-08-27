0293286a761ac7f66fd46d1afd20aeaab0338c46
Stephen Williams <stephenjw@fastmail.com>
2021-08-27 13:27:03 -0400

    get private key from .well-known

--- src/main/java/com/sjwi/meals/service/security/JwtManager.java
+++ src/main/java/com/sjwi/meals/service/security/JwtManager.java
@@ -4,7 +4,7 @@ import java.net.URI;
 import java.util.Arrays;
 import java.util.Base64;
 
-import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
 
 import com.google.gson.Gson;
 import com.sjwi.meals.model.security.AccessTokenResponse;
@@ -17,8 +17,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.impl.crypto.DefaultJwtSignatureValidator;
+import io.jsonwebtoken.Jwts;
 
 @Component
 public class JwtManager {
@@ -32,16 +31,26 @@ public class JwtManager {
     String header = new String(decoder.decode(chunks[0]));
     JwtHeader jwtHeader = new Gson().fromJson(header, JwtHeader.class);
     String privateKey = getPrivateKey(jwtHeader);
-    String payload = new String(decoder.decode(chunks[1]));
-    String tokenWithoutSignature = chunks[0] + "." + chunks[1];
-    String signature = chunks[2];
-    SignatureAlgorithm sa = SignatureAlgorithm.RS256;
-    SecretKeySpec secretKeySpec = new SecretKeySpec(privateKey.getBytes(), sa.getJcaName());
-    DefaultJwtSignatureValidator validator = new DefaultJwtSignatureValidator(sa, secretKeySpec);
-    if (!validator.isValid(tokenWithoutSignature, signature)) {
-      throw new Exception("Could not verify JWT token integrity!");
-    }
-    return payload;
+    System.out.println(privateKey);
+    return Jwts.parser()
+            .setSigningKey(DatatypeConverter.parseBase64Binary(privateKey))
+            .parseClaimsJws(response.getAccess_token()).getBody().toString();
+    // String[] chunks = response.getAccess_token().split("\\.");
+    // Base64.Decoder decoder = Base64.getDecoder();
+    // String header = new String(decoder.decode(chunks[0]));
+    // JwtHeader jwtHeader = new Gson().fromJson(header, JwtHeader.class);
+    // String privateKey = getPrivateKey(jwtHeader);
+    // String payload = new String(decoder.decode(chunks[1]));
+    // String tokenWithoutSignature = chunks[0] + "." + chunks[1];
+    // String signature = chunks[2];
+    // SignatureAlgorithm sa = SignatureAlgorithm.RS256;
+    // System.out.println(privateKey);
+    // SecretKeySpec secretKeySpec = new SecretKeySpec(privateKey.getBytes(), sa.getJcaName());
+    // DefaultJwtSignatureValidator validator = new DefaultJwtSignatureValidator(sa, secretKeySpec);
+    // if (!validator.isValid(tokenWithoutSignature, signature)) {
+    //   throw new Exception("Could not verify JWT token integrity!");
+    // }
+    // return payload;
   }
 
   private String getPrivateKey(JwtHeader jwtHeader) throws Exception {
