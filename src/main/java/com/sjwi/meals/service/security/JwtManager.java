7b1589ff09d1d91172db91c6cb8c0a19095c6411
Stephen Williams <stephenjw@fastmail.com>
2021-08-27 13:50:15 -0400

    j

--- src/main/java/com/sjwi/meals/service/security/JwtManager.java
+++ src/main/java/com/sjwi/meals/service/security/JwtManager.java
@@ -1,14 +1,15 @@
 package com.sjwi.meals.service.security;
 
 import java.net.URI;
-import java.security.KeyFactory;
 import java.security.interfaces.RSAPublicKey;
-import java.security.spec.X509EncodedKeySpec;
 import java.util.Arrays;
-import java.util.Base64;
 
-import javax.xml.bind.DatatypeConverter;
-
+import com.auth0.jwk.Jwk;
+import com.auth0.jwk.JwkProvider;
+import com.auth0.jwk.UrlJwkProvider;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.google.gson.Gson;
 import com.sjwi.meals.model.security.AccessTokenResponse;
 import com.sjwi.meals.model.security.JwtHeader;
@@ -20,8 +21,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
-import io.jsonwebtoken.Jwts;
-
 @Component
 public class JwtManager {
 
@@ -29,40 +28,12 @@ public class JwtManager {
   String clientSecret;
 
   public String getUnpackedAccessToken(AccessTokenResponse response) throws Exception {
-    String[] chunks = response.getAccess_token().split("\\.");
-    Base64.Decoder decoder = Base64.getDecoder();
-    String header = new String(decoder.decode(chunks[0]));
-    JwtHeader jwtHeader = new Gson().fromJson(header, JwtHeader.class);
-    String payload = new String(decoder.decode(chunks[1]));
-    String privateKey = getPrivateKey(jwtHeader);
-
-
-    byte[] keyBytes = DatatypeConverter.parseBase64Binary(privateKey); // your key here
-
-    X509EncodedKeySpec keySpecX509 = new X509EncodedKeySpec(keyBytes);
-    KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-    RSAPublicKey pubKey = (RSAPublicKey) keyFactory.generatePublic(keySpecX509);
-    return Jwts.parser()
-      .setSigningKey(pubKey)
-      .parseClaimsJws(response.getAccess_token()).getBody().toString();
- 
- 
-    // String[] chunks = response.getAccess_token().split("\\.");
-    // Base64.Decoder decoder = Base64.getDecoder();
-    // String header = new String(decoder.decode(chunks[0]));
-    // JwtHeader jwtHeader = new Gson().fromJson(header, JwtHeader.class);
-    // String privateKey = getPrivateKey(jwtHeader);
-    // String payload = new String(decoder.decode(chunks[1]));
-    // String tokenWithoutSignature = chunks[0] + "." + chunks[1];
-    // String signature = chunks[2];
-    // SignatureAlgorithm sa = SignatureAlgorithm.RS256;
-    // System.out.println(privateKey);
-    // SecretKeySpec secretKeySpec = new SecretKeySpec(privateKey.getBytes(), sa.getJcaName());
-    // DefaultJwtSignatureValidator validator = new DefaultJwtSignatureValidator(sa, secretKeySpec);
-    // if (!validator.isValid(tokenWithoutSignature, signature)) {
-    //   throw new Exception("Could not verify JWT token integrity!");
-    // }
-    // return payload;
+    DecodedJWT jwt = JWT.decode(response.getAccess_token());
+    JwkProvider provider = new UrlJwkProvider("https://api.kroger.com/v1");
+    Jwk jwk = provider.get(jwt.getKeyId());
+    Algorithm algorithm = Algorithm.RSA256((RSAPublicKey) jwk.getPublicKey(), null);
+    algorithm.verify(jwt);
+    return jwt.getPayload();
   }
 
   private String getPrivateKey(JwtHeader jwtHeader) throws Exception {
