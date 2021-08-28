0af46329ea31e2d057ec29f1b8e24fe2cbcda7ac
Stephen Williams <stephenjw@fastmail.com>
2021-08-28 02:30:37 -0400

    fire in da hole

--- src/main/resources/static/js/static-functions.js
+++ src/main/resources/static/js/static-functions.js
@@ -277,3 +277,29 @@ function addScrollListener(size, delay){
 		});
 	})
 }
+
+function incrementValue(e) {
+  e.preventDefault();
+  var fieldName = $(e.target).data('field');
+  var parent = $(e.target).closest('div');
+  var currentVal = parseInt(parent.find('input[name=' + fieldName + ']').val(), 10);
+
+  if (!isNaN(currentVal)) {
+      parent.find('input[name=' + fieldName + ']').val(currentVal + 1);
+  } else {
+      parent.find('input[name=' + fieldName + ']').val(0);
+  }
+}
+
+function decrementValue(e) {
+  e.preventDefault();
+  var fieldName = $(e.target).data('field');
+  var parent = $(e.target).closest('div');
+  var currentVal = parseInt(parent.find('input[name=' + fieldName + ']').val(), 10);
+
+  if (!isNaN(currentVal) && currentVal > 0) {
+      parent.find('input[name=' + fieldName + ']').val(currentVal - 1);
+  } else {
+      parent.find('input[name=' + fieldName + ']').val(0);
+  }
+}
